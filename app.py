a3a39b89be6c5b9f10fbb61ccc917e8ee3cdba22
yusankin <67494180+yusankin@users.noreply.github.com>
2023-04-22 22:47:20 +0900

    完成
    WebGLとFlask間の連携完了

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__,static_folder="templates")
 def main_page():
     return render_template("index.html")
 
+@app.route("/SphereMove", methods=["POST"])
+def TempDateToUnity():
+    flaskspeed = 10
+    res = {"count": flaskspeed}
+    return jsonify(res)
+
 if __name__ == "__main__":
     app.run(debug=True , port=8888)
